70dbc87a1d8070d0cb450b7904175e801b17bc49
sclark81 <shawn.clark@jcadm.org>
2026-06-02 13:19:24 -0500

    Fix CollectionView Padding and iOS Entry Point

--- Platforms/iOS/Main.cs
+++ Platforms/iOS/Main.cs
@@ -2,10 +2,10 @@ using UIKit;
 
 namespace JCA.Mobile;
 
-public class Application
+public class Program
 {
 	// This is the main entry point of the application.
-	static void Main(string[] args)
+	public static void Main(string[] args)
 	{
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can provide it here.
