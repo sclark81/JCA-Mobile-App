c80ead41672f12945247a3d6ce2bc70db7893e5b
sclark81 <shawn.clark@jcadm.org>
2026-06-02 13:21:38 -0500

    Fix Android Manifest schema and iOS Program namespace

--- Platforms/iOS/Main.cs
+++ Platforms/iOS/Main.cs
@@ -5,7 +5,7 @@ namespace JCA.Mobile;
 public class Program
 {
 	// This is the main entry point of the application.
-	public static void Main(string[] args)
+	static void Main(string[] args)
 	{
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can provide it here.
