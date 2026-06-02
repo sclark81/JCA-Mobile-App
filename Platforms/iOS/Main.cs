a7211b8d2e37424c0b171126b1ec3de4ccfa64c5
sclark81 <shawn.clark@jcadm.org>
2026-06-02 13:23:07 -0500

    Move AndroidManifest to root and fix iOS entry point signature

--- Platforms/iOS/Main.cs
+++ Platforms/iOS/Main.cs
@@ -4,11 +4,8 @@ namespace JCA.Mobile;
 
 public class Program
 {
-	// This is the main entry point of the application.
-	static void Main(string[] args)
+	public static void Main(string[] args)
 	{
-		// if you want to use a different Application Delegate class from "AppDelegate"
-		// you can provide it here.
 		UIApplication.Main(args, null, typeof(AppDelegate));
 	}
 }
