a606903d3adbb49141720f5f66ba50c6ccaa3885
Shawn Clark <shawn.clark@jcadm.org>
2026-06-03 11:43:36 -0500

    Initial Error Fixes

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,7 +4,8 @@ using Microsoft.Maui;
 
 namespace JCA.Mobile;
 
-[Activity(Theme = "@style/Maui.MainTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.MainTheme.NoActionBar", MainLauncher = true
+    , ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
